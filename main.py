838a2e6e313508754181aa32a0c069a3703cd268
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-12-25 21:17:29 -0800

    adjusted for source having multiple potential pages

--- main.py
+++ main.py
@@ -11,19 +11,32 @@ def download_audio(url: str, name: str):
         f.write(data.content)
 
 
-def main():
-    url = input('CafeAudioBook book URL:\n')
-    page = requests.get(url)
+def download_all_audio_on_page(url: str, page: int):
+    full_url = url
+    if page != 1:
+        full_url += f"/{page}"
 
-    soup = BeautifulSoup(page.content, "html.parser")
+    print(f'Downloading from "page" {page} (URL: {full_url})')
+
+    soup = BeautifulSoup(requests.get(full_url).content, "html.parser")
 
     audio_tag = soup.find_all('audio')
     for audio in audio_tag:
         audio_id = audio['id']
         src = audio.find('source')
         if src and src.get('type', None) == "audio/mpeg" and src.get('src', None) != None:
+            audio_name = src['src'].split('/')[-1].split(".mp")[0]
             print(f"Downloading {audio_id}:\t\t{src['src']}")
-            download_audio(src['src'], audio_id)
+            download_audio(src['src'], audio_name)
+
+
+def main():
+    base_url = input('Enter book URL:\n')
+
+    num_pages = int(input('Number of "pages": '))
+
+    for page in range(1, num_pages):
+        download_all_audio_on_page(base_url, page)
 
 
 if __name__ == '__main__':
